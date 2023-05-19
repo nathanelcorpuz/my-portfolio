857eb2f185f575f21caaa8d39a1aa17dbacad64f
natdlc <nathanelcorpuz@gmail.com>
2023-05-19 19:02:20 +0800

    home hero mobile ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
-import Button from "@/components/Button";
-import HeadingText from "@/components/HeadingText";
-import LinkText from "@/components/LinkText";
-import StandardText from "@/components/StandardText";
+import Hero from "./_components/Hero";
 
 export default function Home() {
 	return (
 		<div>
-			<section className="flex flex-col gap-[30px]">
-				<section className="flex flex-col gap-[5px]">
-					<StandardText>Nathanel Corpuz</StandardText>
-					<HeadingText>Full stack developer</HeadingText>
-					<StandardText variant={1}>
-						Making the web a better place, one line of code at a time
-					</StandardText>
-				</section>
-				<section>
-					<p className="italic text-green-500">
-						Currently looking for a full-time role
-					</p>
-				</section>
-				<section>
-					<Button>Projects</Button>
-				</section>
-				<section className="flex gap-[30px]">
-					<LinkText href="skills">Skills</LinkText>
-					<LinkText href="contact">Contact</LinkText>
-				</section>
-			</section>
+			<Hero />
 		</div>
 	);
 }
