9e8d666521a078aa92c8f242a395361ba8e79c05
nathanelcorpuz <nathanelwebdesign@gmail.com>
2023-05-20 07:25:54 +0800

    spacing test

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import SkillsOverview from "./_components/SkillsOverview";
 import Technologies from "./_components/Technologies";
 
 export default function Home() {
-	return (
-		<div className="flex flex-col gap-[100px]">
-			<Hero />
-			<SkillsOverview />
-			<Technologies />
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-[100px]">
+      <Hero />
+      <SkillsOverview />
+      <Technologies />
+    </div>
+  );
 }
