073434f16384dcfff8c3fab7e71c009aaa92d7b4
natdlc <nathanelcorpuz@gmail.com>
2023-05-19 18:59:55 +0800

    link text

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
 import Button from "@/components/Button";
 import HeadingText from "@/components/HeadingText";
+import LinkText from "@/components/LinkText";
 import StandardText from "@/components/StandardText";
 
 export default function Home() {
-  return (
-    <div>
-      <section className="flex flex-col gap-[20px]">
-        <section className="flex flex-col gap-[5px]">
-          <StandardText>Nathanel Corpuz</StandardText>
-          <HeadingText>Full stack developer</HeadingText>
-          <StandardText variant={1}>
-            Passionate in solving business problems through high quality
-            JavaScript code.
-          </StandardText>
-        </section>
-        <section>
-          <p className="italic text-green-500">
-            Currently looking for a full-time role
-          </p>
-        </section>
-        <section>
-          <Button>Projects</Button>
-        </section>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			<section className="flex flex-col gap-[30px]">
+				<section className="flex flex-col gap-[5px]">
+					<StandardText>Nathanel Corpuz</StandardText>
+					<HeadingText>Full stack developer</HeadingText>
+					<StandardText variant={1}>
+						Making the web a better place, one line of code at a time
+					</StandardText>
+				</section>
+				<section>
+					<p className="italic text-green-500">
+						Currently looking for a full-time role
+					</p>
+				</section>
+				<section>
+					<Button>Projects</Button>
+				</section>
+				<section className="flex gap-[30px]">
+					<LinkText href="skills">Skills</LinkText>
+					<LinkText href="contact">Contact</LinkText>
+				</section>
+			</section>
+		</div>
+	);
 }
---
+++ components/LinkText.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function LinkText({
+	children,
+	href,
+	...props
+}: {
+	children: React.ReactNode;
+	href: string;
+}) {
+	return (
+		<Link
+			className="uppercase font-light underline underline-offset-8 tracking-widest"
+			{...props}
+			href={href}
+		>
+			{children}
+		</Link>
+	);
+}
