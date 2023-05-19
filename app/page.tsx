5fc302de1d6a61d51fd0c07cfea6ee23d6332681
nathanelcorpuz <nathanelwebdesign@gmail.com>
2023-05-19 16:35:08 +0800

    standard text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
 import HeadingText from "@/components/HeadingText";
+import StandardText from "@/components/StandardText";
 
 export default function Home() {
-  return <div>
-    <HeadingText>Full stack developer</HeadingText>
-  </div>;
+  return (
+    <div>
+      <section className="flex flex-col gap-[20px]">
+        <section className="flex flex-col gap-[5px]">
+          <StandardText>Nathanel Corpuz</StandardText>
+          <HeadingText>Full stack developer</HeadingText>
+          <StandardText variant={1}>
+            Passionate in solving business problems through high quality
+            JavaScript code.
+          </StandardText>
+        </section>
+        <section>
+          <p className="italic text-green-500">
+            Currently looking for a full-time role
+          </p>
+        </section>
+        <section>
+          <button>Projects</button>
+        </section>
+      </section>
+    </div>
+  );
 }
---
+++ components/StandardText.tsx
@@ -0,0 +1,18 @@
+import { StandardTextVariant } from "@/types/types";
+
+export default function StandardText({
+  children,
+  variant = 2,
+}: {
+  children: React.ReactNode;
+  variant?: StandardTextVariant;
+}) {
+  const common = "text-gray-300";
+  if (variant === 1) {
+    return <p className={`${common}`}>{children}</p>;
+  }
+  if (variant === 3) {
+    return <p className={`${common} text-xs`}>{children}</p>;
+  }
+  return <p className={`${common} text-sm`}>{children}</p>;
+}
