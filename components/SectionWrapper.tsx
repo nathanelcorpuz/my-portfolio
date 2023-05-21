25cc5c1cc4694d4be727ffe623a7fd042e57cf1c
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-21 09:50:59 +0800

    section wrapper spacing

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -8,10 +8,10 @@ export default function SectionWrapper({
   gapSize?: GapSize;
 }) {
   if (gapSize === "md") {
-    return <section className="flex flex-col gap-[50px]">{children}</section>;
+    return <section className="p-4 flex flex-col gap-[50px]">{children}</section>;
   }
   if (gapSize === "lg") {
-    return <section className="flex flex-col gap-[100px]">{children}</section>;
+    return <section className="p-4 flex flex-col gap-[100px]">{children}</section>;
   }
-  return <section className="flex flex-col gap-[25px]">{children}</section>;
+  return <section className="p-4 flex flex-col gap-[25px]">{children}</section>;
 }
