045f4b2bab041446e129082cafae23159872ea44
nathanelcorpuz <nathanelcorpuz@gmail.com>
2023-05-22 10:44:04 +0800

    root layout ui fix

--- app/_components/RootLayoutWrapper.tsx
+++ app/_components/RootLayoutWrapper.tsx
@@ -1,20 +1,11 @@
-"use client";
-
-import { ThemeContext } from "@/providers/ThemeProvider";
-import { Theme } from "@/types/types";
-import { useContext } from "react";
-
 export default function RootLayoutWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { theme }: Theme = useContext(ThemeContext);
   return (
     <div
       className={`
-      max-w-[550px] sm:max-w-[800px]
-      lg:max-w-[1200px] xl:max-w-[1500px] 
       [min-height:100vh] flex flex-col 
       gap-[100px] justify-between bg-white dark:bg-black 
       }
